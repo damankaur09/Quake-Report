7e803e23bb2581188ef9f1a6518ea821682fc6e7
damankaur09 <damankaur209@gmail.com>
2016-12-11 21:38:07 +0530

    Add order-by preference

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -127,13 +127,15 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderManag
         SharedPreferences sharedPrefs= PreferenceManager.getDefaultSharedPreferences(this);
         String minMag=sharedPrefs.getString(getString(R.string.settings_min_magnitude_key),getString(R.string.settings_min_magnitude_default));
 
+        String orderBy=sharedPrefs.getString(getString(R.string.settings_order_by_key),getString(R.string.settings_order_by_default));
+
         Uri baseUri=Uri.parse(SAMPLE_JSON_RESPONSE);
         Uri.Builder uriBuilder=baseUri.buildUpon();
 
         uriBuilder.appendQueryParameter("format","geojson");
         uriBuilder.appendQueryParameter("limit","10");
         uriBuilder.appendQueryParameter("minmag",minMag);
-        uriBuilder.appendQueryParameter("orderby","time");
+        uriBuilder.appendQueryParameter("orderby",orderBy);
 
         return new EarthquakeLoader(this,uriBuilder.toString());
     }
